c8c8f81c899424f0bfb1c69026bb9205543108ef
rembouyss <remibouyss@gmail.com>
2023-12-19 18:55:00 +0100

    on génère les questions et on checke les réponses. Score se met à jour

--- config.js
+++ config.js
@@ -2,4 +2,18 @@ const verbes = ['lire','écrire','mettre', 'penser']
 const infinitif = ['read','write','put','think']
 const preterit = ['read','wrote','put','thought']
 const partPasse = ['read','written','put','thought']
-let questions = ""
+let reponses = ""
+
+// retourne un entier aléatoire entre min et max
+function getRnd(min,max) {
+    let alea = Math.random()    // un nombre décimal entre 0 et 1
+    alea *= max-min+1           // entre 0 et max-min+1
+    alea += min                 // entre min et max+1
+    alea = Math.floor(alea)     // entier entre min et max
+    return alea
+}
+
+// retourne un élément aléatoire d'une liste
+function getRndList(liste) {
+    return liste[getRnd(0,liste.length-1)]
+}
--- main.js
+++ main.js
@@ -1,22 +1,53 @@
 
 // On regarde ce qui a été choisi lors du clic sur le bouton
-let btnOK = document.getElementById("btnOK")
+let btnOK = document.getElementById("btnValiderChoix")
 
 btnOK.addEventListener("click", () => {
     let listeRadio = document.querySelectorAll("#choix input")
     let i=0
     while (!listeRadio[i].checked) i++
     reponses = choisirReponses(listeRadio[i].value)
+    determinerConsigne(listeRadio[i].value)
     lancerJeu()
 })
 
-// détermine si on va demander des infinitifs, Prétérits ou paticipe passés
+// détermine si on va demander des infinitifs, prétérits ou paticipe passés
 function choisirReponses(choix) {
     if (choix === "0") return infinitif
     if (choix === "1") return preterit
     return partPasse
 }
 
-function lancerJeu() {
+// gère ce qui est écrit, la consigne
+function determinerConsigne(choix) {
+    let possibilites = ["'infinitif","e prétérit","e participe passé"];
+    let consigne = document.getElementById("consigne");
+    consigne.innerText = `Donne l${possibilites[choix]} des verbes suivants en anglais`;
+}
 
+function lancerJeu() {
+    let score = 0
+    let cpt = 0
+    let question = document.getElementById("question")
+    let index = getRnd(0,verbes.length-1)
+    question.innerText = verbes[index]
+
+    let btnValiderReponse = document.getElementById("btnValiderReponse")
+
+    btnValiderReponse.addEventListener("click", () => {
+        cpt++;
+
+        let reponse = document.querySelector("#zoneSaisie input");
+        if (reponse.value === reponses[index]) score++;
+        reponse.value = ""
+        
+        let scoreLabel = document.getElementById("scoreLabel");
+        console.log(scoreLabel)
+        scoreLabel.innerText = `Score : ${score} / ${cpt}`;
+
+        index = getRnd(0,verbes.length-1);
+        question.innerText = verbes[index];
+    })
+    
+    
 }
